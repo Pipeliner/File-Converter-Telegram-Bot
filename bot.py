136bf4ced5f6c9977662232367bf2ec933c90e95
Vadim Evard <v.e.evard@gmail.com>
2020-03-12 21:27:04 +0300

    Implemented convert-to-pdf

--- bot.py
+++ bot.py
@@ -1,34 +1,40 @@
-# -*- coding: utf-8 -*-
+import convertapi
 import os
 import telebot
-# import some_api_lib
-# import ...
+import tempfile
+import requests
+from io import BytesIO
 
-# Example of your code beginning
-#           Config vars
 token = os.environ['TELEGRAM_TOKEN']
-#             ...
-
+convertapi.api_secret = os.environ['CONVERTAPI_SECRET']
 
 bot = telebot.TeleBot(token)
-keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Привет', 'Пока')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Please send me your files')
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
-    elif message.text.lower() == 'я тебя люблю':
-        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
-
-@bot.message_handler(content_types=['sticker'])
-def sticker_id(message):
+@bot.message_handler(content_types=['document', 'audio', 'photo', 'video'])
+def convert_to_pdf(message):
     print(message)
+    content_type = message.content_type
+
+    documents = getattr(message, content_type)
+    if not isinstance(documents, list):
+        documents = [documents]
+
+    for doc in documents:
+        file_id = doc.file_id
+        # bot.send_document(message.chat.id, file_id)
+        file_info = bot.get_file(file_id)
+        file_url = 'https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path)
+
+        file_data = BytesIO(requests.get(file_url).content)
+        upload_io = convertapi.UploadIO(file_data, filename=doc.file_name)
+        converted_result = convertapi.convert('pdf', {'File': upload_io})
+        converted_files = converted_result.save_files(tempfile.gettempdir())
+
+        for file in converted_files:
+            bot.send_document(message.chat.id, open(file, 'rb'))
 
 bot.polling()
